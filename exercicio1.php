c3cf953362892e8e4b6f11d337f4867035810641
gabrielsantos2019 <48133473+gabrielsantos2019@users.noreply.github.com>
2019-03-16 17:15:01 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -1,2 +1,12 @@
-<?php
+<?php 
+
+    //conversão de metros para milímetros
+
+    print("Insira um valor em metros: ");
+    $m = fgets(STDIN);
+
+    $mili = $m*1000;
+
+    print("O valor em milimetros é: $mili");
+
 
